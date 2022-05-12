ded9834612ddf58e08d9189096056d14163d8886
QuaKa4 <104523779+QuaKa4@users.noreply.github.com>
2022-05-12 20:14:35 +0300

    function about summation data from list LIst_data

---
+++ main.py
@@ -0,0 +1,3 @@
+list_data = ['12', '12', '98', '634', 'jdfs']
+for element in list_data:
+    print(f'{element} kgkgkg')
---
+++ test.py
@@ -0,0 +1,11 @@
+list_data = ['12', 12, '98', '634', 'jdfs']
+for element in list_data:
+    print(f'{element} kgkgkg')
+
+
+def all_data(data1, data2):
+    sum_data = int(data1) + data2
+    print(sum_data)
+
+
+all_data(int(list_data[0]), list_data[1])

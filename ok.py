4f6f1a945c2c19dbce1dbcea26f083ff8b8f5fc4
QuaKa4 <104523779+QuaKa4@users.noreply.github.com>
2022-05-17 21:19:33 +0300

    function about summation data from list LIst_data

---
+++ ok.py
@@ -0,0 +1,9 @@
+def lmao(**kwargs):
+    ae = kwargs
+    ui = ae.get('an')
+    print(ui)
+
+
+lmao(jae="495", hflf="04780", dldlf="984")
+lmao(an="958", hflf="23980", dlfjdl="94028")
+lmao(an="498", hflf="2394780", djdlf="98284")

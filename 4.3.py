bf7e5383ea8b790522efa0fa7a9033cedf96299a
QuaKa4 <104523779+QuaKa4@users.noreply.github.com>
2022-05-19 19:05:25 +0300

    function about summation data from list LIst_data

--- 4.3.py
+++ 4.3.py
@@ -58,8 +58,31 @@ for i in config:
         break
 
 ola = wlist[2] + wlist[3]
-wlist.remove(wlist[3])
-wlist.remove(wlist[4])
-wlist.insert(2, ola)
 
-print(wlist)
+wlist.remove(wlist[3])
+wlist.remove(wlist[3])
+wlist.insert(2, ola)
+wlist[3] = '2'
+
+ola2 = wlist[3] + wlist[4]
+wlist.remove(wlist[4])
+wlist.remove(wlist[3])
+wlist.insert(3, ola2)
+
+ola3 = wlist[4] + wlist[5]
+wlist.remove(wlist[5])
+wlist.remove(wlist[6])
+wlist.insert(4, ola3)
+
+wlist[5] = '1'
+wlist[6] = '0'
+
+ola4 = wlist[5] + wlist[6] + wlist[7]
+wlist.remove(wlist[7])
+wlist.remove(wlist[6])
+wlist.remove(wlist[5])
+wlist.remove(wlist[4])
+wlist.insert(5, ola4)
+wlist.insert(0, '1')
+
+print(*wlist)
